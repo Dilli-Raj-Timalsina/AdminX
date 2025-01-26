3a536075abaecfac614ec51f737b97d3af4bdc13
Dilli-Raj-Timalsina <dillirajtimalsina354@gmail.com>
2025-01-27 00:43:10 +0545

    chore: extras .env types added

--- backend/@types/.env.d.ts
+++ backend/@types/.env.d.ts
@@ -1,5 +1,11 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    HOST: string;
+    CORS_ORIGIN: string;
+    COMMON_RATE_LIMIT_WINDOW_MS: string;
+    COMMON_RATE_LIMIT_MAX_REQUESTS: string;
     DB_TYPE: string;
     DB_HOST: string;
     DB_PORT: number;
--- backend/src/common/utils/envConfig.ts
+++ backend/src/common/utils/envConfig.ts
@@ -4,10 +4,20 @@ import { cleanEnv, host, num, port, str, testOnly } from "envalid";
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ devDefault: testOnly("test"), choices: ["development", "production", "test"] }),
+  NODE_ENV: str({
+    devDefault: testOnly("test"),
+    choices: ["development", "production", "test"],
+  }),
   HOST: host({ devDefault: testOnly("localhost") }),
   PORT: port({ devDefault: testOnly(3000) }),
   CORS_ORIGIN: str({ devDefault: testOnly("http://localhost:3000") }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
+
+  DB_TYPE: str({ devDefault: testOnly("postgres") }),
+  DB_HOST: str({ devDefault: testOnly("localhost") }),
+  DB_PORT: num({ devDefault: testOnly(5432) }),
+  DB_USERNAME: str({ devDefault: testOnly("postgres") }),
+  DB_PASSWORD: str({ devDefault: testOnly("randompassword") }),
+  DB_NAME: str({ devDefault: testOnly("crud-test-db") }),
 });
